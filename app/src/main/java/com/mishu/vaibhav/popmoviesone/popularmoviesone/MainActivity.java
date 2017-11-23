361ea167a3c7f87b00fe989c6f346d17aec66c9e
mishuvs <weatherapprobux@gmail.com>
2017-11-24 02:19:54 +0530

    Add basic configuration in the MainActivity & activity_main for recyclerView

--- app/src/main/java/com/mishu/vaibhav/popmoviesone/popularmoviesone/MainActivity.java
+++ app/src/main/java/com/mishu/vaibhav/popmoviesone/popularmoviesone/MainActivity.java
@@ -2,6 +2,8 @@ package com.mishu.vaibhav.popmoviesone.popularmoviesone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MovieAdapter adapter = new MovieAdapter();
+        GridLayoutManager movieLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        RecyclerView movieRecyclerView = findViewById(R.id.movie_recycler_view);
+        movieRecyclerView.setAdapter(adapter);
+        movieRecyclerView.setLayoutManager(movieLayoutManager);
+
     }
 }
